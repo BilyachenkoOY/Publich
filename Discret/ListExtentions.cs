cf1a0fb31c5b2ea242ba24caa5ad70d4be9613fa
BilyachenkoOY <iviviv1901@gmail.com>
2015-09-29 22:27:23 +0300

    type

--- Discret/ListExtentions.cs
+++ Discret/ListExtentions.cs
@@ -8,7 +8,7 @@ namespace Discret
 {
     public static class ListExtentions
     {
-        public static List<Boolean> And(this List<Boolean> x, List<Boolean> y){
+        public static IList<Boolean> And(this IList<Boolean> x, IList<Boolean> y){
             if (x.Count != y.Count) throw new ArgumentException("diff lenght");
             var res = new List<Boolean>(x.Count);
             for (Int16 i = 0; i < x.Count; i++)
@@ -18,7 +18,7 @@ namespace Discret
             return res;
         }
 
-        public static List<Boolean> Or(this List<Boolean> x, List<Boolean> y)
+        public static IList<Boolean> Or(this IList<Boolean> x, IList<Boolean> y)
         {
             if (x.Count != y.Count) throw new ArgumentException("diff lenght");
             var res = new List<Boolean>(x.Count);
@@ -29,7 +29,7 @@ namespace Discret
             return res;
         }
 
-        public static List<Boolean> Not(this List<Boolean> x)
+        public static IList<Boolean> Not(this IList<Boolean> x)
         {
             var res = new List<Boolean>(x.Count);
             for (Int16 i = 0; i < x.Count; i++)
